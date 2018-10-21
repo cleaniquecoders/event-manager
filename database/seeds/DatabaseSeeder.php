5685cd24be866853f6bb45c5a3ffed4acecd2982
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-21 12:48:24 +0800

    Initial Commit

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+	use \App\Traits\SeedingProgressBar;
+
+	public $seeders = [
+		RolesAndPermissionsSeeder::class => true,
+	];
+}
