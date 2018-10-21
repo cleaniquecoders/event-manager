9d44810fb776a3bb9223b6d44d3edb728e90cbf1
Nasrul Hazim <nasrulhazim.m@gmail.com>
2018-10-21 19:20:29 +0800

    Remove extra cross on modal

--- resources/views/components/modals/base.blade.php
+++ resources/views/components/modals/base.blade.php
@@ -4,7 +4,7 @@
       <div class="modal-header">
         <h5 class="modal-title" id="{{ $id }}ModalLongTitle">{{ __($modal_title) }}</h5>
         <button type="button" class="close" data-dismiss="modal" aria-label="{{ __('Close') }}">
-          <span aria-hidden="true">&times;</span>
+          
         </button>
       </div>
       <div class="modal-body">
